ad03fa7e7f02623e203b7c0b0143901a16d80723
Andraž Holešek <132955574+Unh0li@users.noreply.github.com>
2026-05-03 20:19:00 +0200

    Update test_primer.py

--- tests/test_primer.py
+++ tests/test_primer.py
@@ -1,1 +1,10 @@
 
+# tests/test_primer.py
+
+def test_prazno():
+    # Preprost test, ki vedno uspe
+    assert True
+
+def test_sestevanje():
+    # Preveri, če tvoja logika deluje
+    assert 1 + 1 == 2
